cec43abeb5cfa1a64de6577b53348ec5b0b11a39
danwill <daniel.willett2@gmail.com>
2023-03-24 10:35:03 -0700

    added heading and navbar to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,17 @@
 import React from 'react'
 
-import '../styles/Header.css'
+function Header() {
+    return (
+        <header>
+            <h1>Dan Willett</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Resume</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
