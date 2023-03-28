9a21b462854f18d1bfbf9a7288a5520681c4fbbe
danwill <daniel.willett2@gmail.com>
2023-03-28 12:01:11 -0700

    added error prompts to contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,60 +1,116 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 export default function Contact() {
+  const [messenger, setMessenger] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+  const [alert, setAlert] = useState("");
+  const [sent, setSent] = useState("");
 
-    const handleInputChange = (e) => {
-        const {field, value} = e.target;
-        if (field === 'name') {
-            return setName(value)
-        } else if (field === 'email') {
-            return setEmail(value)
-        } else if (field === 'message') {
-            return setMessage(value)
-        }
+  const handleInputChange = (e) => {
+    setSent("");
+    const { name, value } = e.target;
+    // if blank, send message saying "this field is required"
+    if (value.length === 0) {
+      setAlert(`Please include all fields`);
+    } else {
+      setAlert("");
+    }
+    if (name === "messenger") {
+      console.log(name);
+      console.log(value);
+      setMessenger(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "message") {
+      setMessage(value);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setAlert("Email is invalid");
+      return;
     }
 
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        alert(`Message sent!`)
-        setName('');
-        setEmail('');
-        setMessage('');
-    }
+    setSent(`Message sent!`);
+    setMessenger("");
+    setEmail("");
+    setMessage("");
+  };
 
-    return (
-        <div>
-            <p>Please contact me through the form below.</p>
-            <form className="form">
-                <input 
-                value={name}
-                name="name"
-                onChange={handleInputChange}
-                type="text"
-                placeholder="name"
-                />
-                <input 
-                value={email}
-                name="email"
-                onChange={handleInputChange}
-                type="text"
-                placeholder="email"
-                />
-                <input 
-                value={message}
-                name="message"
-                onChange={handleInputChange}
-                type="text"
-                placeholder="message"
-                />
-                <button type="button" onClick={handleFormSubmit}>
-                    Submit
-                </button>
-            </form>
+  const styles = {
+    form: {
+      padding: "0 20px",
+      lineHeight: "40px",
+    },
+  };
+
+  return (
+    <div style={styles.form}>
+      <p>Please contact me through the form below.</p>
+      <form>
+        <div className="form-row">
+          <div className="form-group col-4">
+            <label for="messenger">Name</label>
+            <input
+              id="messenger"
+              className="form-control"
+              value={messenger}
+              name="messenger"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="form-group col-4">
+            <label for="email">Email Address</label>
+            <input
+              id="email"
+              className="form-control"
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+              placeholder="email"
+            />
+          </div>
         </div>
-    )
-    
+
+        <div className="form-group col-10">
+          <label for="message">Message</label>
+          <input
+            id="message"
+            className="form-control"
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Insert your message here"
+          />
+        </div>
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={handleFormSubmit}
+        >
+          Submit
+        </button>
+      </form>
+      {alert && (
+        <div className="alert alert-danger" role="alert">
+          {alert}
+        </div>
+      )}
+      {sent && (
+        <div className="alert alert-success" role="alert">
+          {sent}
+        </div>
+      )}
+    </div>
+  );
 }
