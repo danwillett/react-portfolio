07d412925b732431832f00b0dbf1cbf499b41015
danwill <daniel.willett2@gmail.com>
2023-03-28 10:57:58 -0700

    added form html features and helper function to validate email address

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Contact() {
-    <form>
-        <h3>contact me now</h3>
-    </form>
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const {field, value} = e.target;
+        if (field === 'name') {
+            return setName(value)
+        } else if (field === 'email') {
+            return setEmail(value)
+        } else if (field === 'message') {
+            return setMessage(value)
+        }
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        alert(`Message sent!`)
+        setName('');
+        setEmail('');
+        setMessage('');
+    }
+
+    return (
+        <div>
+            <p>Please contact me through the form below.</p>
+            <form className="form">
+                <input 
+                value={name}
+                name="name"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="name"
+                />
+                <input 
+                value={email}
+                name="email"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="email"
+                />
+                <input 
+                value={message}
+                name="message"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="message"
+                />
+                <button type="button" onClick={handleFormSubmit}>
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
+    
 }
